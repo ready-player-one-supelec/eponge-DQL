9c6d817de0b02a3e972261f9638146af2e2c74c2
Nathan Cassereau <nathan.cassereau@supelec.fr>
2018-12-28 21:55:21 +0100

    Better QNetwork

--- batonnets/main.py
+++ batonnets/main.py
@@ -5,5 +5,6 @@ from game import *
 from player import *
 
 nbSticks = 12
+learningRate = 0.01
 game = Game(nbSticks)
-player = Player("Toto", True, nbSticks)
+player = Player("Toto", True, learningRate)
--- batonnets/player.py
+++ batonnets/player.py
@@ -6,30 +6,42 @@ import numpy as np
 
 class Player :
 
-    def __init__(self, name, isBot, originalNumberSticks) :
+    def __init__(self, name, isBot, learning_rate) :
         self.name = name
         self.isBot = isBot
-        self.QNetwork, self.input = self.createNeuralNetwork(originalNumberSticks)
+        self.learningRate = learning_rate
 
-        self.test([[0,0,0,0]])
+        self.createQNetwork()
+        self.createOptimiser()
 
-    def createNeuralNetwork(self, nbSticks) :
-        input = tf.placeholder(tf.float32, shape=[None,4])
+        self.evaluate([[1, 2, 3, 4]])
+
+    def createQNetwork(self) :
+        # input layer
+        self.x = tf.placeholder(tf.float32, [None, 4])
+        # expected output placeholder
+        self.y = tf.placeholder(tf.float32, [None, 3])
 
         # hidden layer
-        W1 = tf.Variable(tf.random_normal([4, 4], stddev=1), name='W1')
-        b1 = tf.Variable(tf.random_normal([4]), name='b1')
-        hiddenLayer = tf.nn.relu(tf.add(tf.matmul(input,W1), b1))
+        self.w1 = tf.Variable(tf.random_normal([4, 4], stddev=1), name='W1')
+        self.b1 = tf.Variable(tf.random_normal([4]), name='b1')
+        self.hiddenLayer = tf.nn.relu(tf.add(tf.matmul(self.x, self.w1), self.b1))
 
         # output layer
-        W2 = tf.Variable(tf.random_normal([4, 3], stddev=1), name='W2')
-        b2 = tf.Variable(tf.random_normal([3]), name="b2")
-        output = tf.add(tf.matmul(hiddenLayer, W2), b2)
-        return output, input
+        self.w2 = tf.Variable(tf.random_normal([4, 3], stddev=1), name='W2')
+        self.b2 = tf.Variable(tf.random_normal([3]), name="b2")
+        self.y_ = tf.add(tf.matmul(self.hiddenLayer, self.w2), self.b2)
 
-    def test(self, testData) :
-        init = tf.global_variables_initializer()
+    def createOptimiser(self) :
+        # is this loss ?
+        self.cost = tf.losses.mean_squared_error(self.y, self.y_)
+        # Gradient Descent Optimiser definition
+        self.optimiser = tf.train.GradientDescentOptimizer(learning_rate=self.learningRate).minimize(self.cost)
+
+        self.init_op = tf.global_variables_initializer()
+
+    def predict(self, input) :
         with tf.Session() as sess :
-            sess.run(init)
-            x = sess.run(self.QNetwork, feed_dict={self.input: testData})
-            print(x)
+            sess.run(self.init_op)
+            prediction = sess.run(self.y_, feed_dict={self.x: input})
+        return prediction

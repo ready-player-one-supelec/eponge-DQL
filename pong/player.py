a2533a7d13035b896bdf014f19503eac82d5f07b
Nathan Cassereau <nathan.cassereau@supelec.fr>
2019-01-28 14:16:52 +0100

    The transition is stored iff is training (to save RAM)

--- pong/player.py
+++ pong/player.py
@@ -117,10 +117,11 @@ class Player :
 
     def addStateSequence(self, action, reward, nextState) :
         nS = self.processor.process(nextState)
-        self.trainingData.append([self.buffer, action, reward, nS])
+        if self.trainable :
+            self.trainingData.append([self.buffer, action, reward, nS])
+            while len(self.trainingData) > self.maxBatchSize :
+                self.trainingData.pop(0)
         self.buffer = nS
-        while len(self.trainingData) > self.maxBatchSize :
-            self.trainingData.pop(0)
 
     def saveQNetwork(self, path, global_step = None) :
         self.QNetwork.saveQNetwork(path, global_step)

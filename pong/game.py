a45ad195cea5759ab2204c67d81db4119f3e2180
Paul Le Grand Des Cloizeaux <paul.legranddec@gmail.com>
2019-02-15 02:52:58 +0100

    removed sleep

--- pong/game.py
+++ pong/game.py
@@ -28,7 +28,6 @@ class Game :
         assert(isinstance(display, bool))
         if self.display :
             self.env.render()
-            time.sleep(0.02)
         observation, reward, done, info = self.env.step(self.possibleActions[action])
         self.observation = observation
         return observation, reward, done

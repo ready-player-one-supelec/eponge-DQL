b15b910eaaa020e6912b9ad3dbf7086f8de9dc56
Nathan Cassereau <nathan.cassereau@supelec.fr>
2019-05-22 12:45:26 +0200

    Reward method updated

--- cartpole/game.py
+++ cartpole/game.py
@@ -14,6 +14,8 @@ class Game :
         # respectively doing nothing, going up and going down
         self.possibleActions = [0, 1]
 
+        self.c = 0
+
     def __enter__(self) :
         self.env = gym.make('CartPole-v1')
         self.reset()
@@ -30,12 +32,17 @@ class Game :
             self.env.render()
             # time.sleep(0.02)
         observation, reward, done, info = self.env.step(self.possibleActions[action])
-        reward = 1 if not done else -1
+        self.c += 1
+        if done and self.c < 500 :
+            reward = -1
+        else :
+            reward = 1
         self.observation = observation
         return observation, reward, done
 
     def reset(self) :
         self.observation = self.env.reset()
+        self.c = 0
 
     def random_step(self) :
         return self.step(random.randrange(0,2))
